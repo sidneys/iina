c5f9bca80fdcf67374405bceb88662ac5238ef3d
sidneys <sidneys.github.io@outlook.com>
2019-05-12 14:40:10 +0200

    feat(playlist-thumbnails): adds `transitionTo`, `resizeTo` convenience methods to `NSImageView`/`NSImage` extensions

--- iina/Extensions.swift
+++ iina/Extensions.swift
@@ -595,3 +595,52 @@ extension NSAppearance {
     }
   }
 }
+
+extension NSImageView {
+  /**
+   Crossfades from `NSImageViews'` current image to a new image.
+   Uses a predefined animation duration.
+
+   - parameters:
+   - image: New image to transition to.
+   - duration: Transition duration interval in seconds.
+   */
+  func transitionTo(image: NSImage, duration: CFTimeInterval) {
+    let transition = CATransition()
+    transition.duration = duration
+    transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+    transition.type = CATransitionType.fade
+    wantsLayer = true
+    layer?.add(transition, forKey: kCATransition)
+    self.image = image
+  }
+}
+
+extension NSImage {
+  /**
+   Resizes `NSImage` across standard- and HiDPI (Retina) screens.
+
+   - parameters:
+   - newSize: The target dimensions.
+   - Returns: The resized `NSImage`.
+   */
+  func resizeTo(to newSize: NSSize) -> NSImage? {
+    if let bitmapRep = NSBitmapImageRep(
+      bitmapDataPlanes: nil, pixelsWide: Int(newSize.width), pixelsHigh: Int(newSize.height),
+      bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false,
+      colorSpaceName: .calibratedRGB, bytesPerRow: 0, bitsPerPixel: 0
+      ) {
+      bitmapRep.size = newSize
+      NSGraphicsContext.saveGraphicsState()
+      NSGraphicsContext.current = NSGraphicsContext(bitmapImageRep: bitmapRep)
+      draw(in: NSRect(x: 0, y: 0, width: newSize.width, height: newSize.height), from: .zero, operation: .copy, fraction: 1.0)
+      NSGraphicsContext.restoreGraphicsState()
+
+      let resizedImage = NSImage(size: newSize)
+      resizedImage.addRepresentation(bitmapRep)
+      return resizedImage
+    }
+
+    return nil
+  }
+}
